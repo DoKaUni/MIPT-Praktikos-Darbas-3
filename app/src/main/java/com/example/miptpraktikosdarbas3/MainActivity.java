c4917523fb112d85ba3761d036eaa1f55f5a6ff8
DoKaUni <dovydas.kasulis@gmail.com>
2023-11-05 15:39:28 +0200

    Added temporary button click logic, changed 'back', 'plusMinus', 'squareRoot' ImageButtons to Buttons with text or icons

--- app/src/main/java/com/example/miptpraktikosdarbas3/MainActivity.java
+++ app/src/main/java/com/example/miptpraktikosdarbas3/MainActivity.java
@@ -11,12 +11,12 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     double memoryResult;
     TextView calculatorText;
     Button buttonMC, buttonMR, buttonMS, buttonMPlus, buttonMMinus,
             buttonBack, buttonCE, buttonC, buttonPlusMinus, buttonSquareRoot,
-            buttonDivision, buttonPercentage, buttonMultiplication, buttonFraction, buttonAddition,
+            buttonDivision, buttonPercent, buttonMultiplication, buttonFraction, buttonAddition,
             buttonSubtraction, buttonDecimalPoint, buttonEquals, buttonN1, buttonN2,
             buttonN3, buttonN4, buttonN5, buttonN6, buttonN7, buttonN8, buttonN9, buttonN0;
 
@@ -24,6 +24,37 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculatorText = findViewById(R.id.calculatorText);
+
+        assignButtonId(buttonMC, R.id.MC);
+        assignButtonId(buttonMR, R.id.MR);
+        assignButtonId(buttonMS, R.id.MS);
+        assignButtonId(buttonMPlus, R.id.MPlus);
+        assignButtonId(buttonMMinus, R.id.MMinus);
+        assignButtonId(buttonBack, R.id.back);
+        assignButtonId(buttonCE, R.id.CE);
+        assignButtonId(buttonC, R.id.C);
+        assignButtonId(buttonPlusMinus, R.id.plusMinus);
+        assignButtonId(buttonSquareRoot, R.id.squareRoot);
+        assignButtonId(buttonDivision, R.id.division);
+        assignButtonId(buttonPercent, R.id.percent);
+        assignButtonId(buttonMultiplication, R.id.multiplication);
+        assignButtonId(buttonFraction, R.id.fraction);
+        assignButtonId(buttonAddition, R.id.addition);
+        assignButtonId(buttonSubtraction, R.id.subtraction);
+        assignButtonId(buttonDecimalPoint, R.id.decimalPoint);
+        assignButtonId(buttonEquals, R.id.equals);
+        assignButtonId(buttonN1, R.id.n1);
+        assignButtonId(buttonN2, R.id.n2);
+        assignButtonId(buttonN3, R.id.n3);
+        assignButtonId(buttonN4, R.id.n4);
+        assignButtonId(buttonN5, R.id.n5);
+        assignButtonId(buttonN6, R.id.n6);
+        assignButtonId(buttonN7, R.id.n7);
+        assignButtonId(buttonN8, R.id.n8);
+        assignButtonId(buttonN9, R.id.n9);
+        assignButtonId(buttonN0, R.id.n0);
 
         String expressionString = "2 + 3 * 1/4"; // Example expression
 
@@ -37,5 +68,20 @@ public class MainActivity extends AppCompatActivity{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    void assignButtonId(Button button, int id){
+        button = findViewById(id);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+        String buttonText = button.getText().toString();
+        String calcDataString = calculatorText.getText().toString();
+        calcDataString += buttonText;
+
+        calculatorText.setText(calcDataString);
     }
 }
