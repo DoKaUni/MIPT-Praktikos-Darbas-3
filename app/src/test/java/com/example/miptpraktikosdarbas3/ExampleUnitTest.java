9ba4d686ab4412b8ef7ba336820ebc1a16201fa9
DoKaUni <dovydas.kasulis@gmail.com>
2023-12-19 19:45:01 +0200

    Added unit tests to all calculator operations

--- app/src/test/java/com/example/miptpraktikosdarbas3/ExampleUnitTest.java
+++ app/src/test/java/com/example/miptpraktikosdarbas3/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,7 +14,59 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testAddition() {
+        String calcDataString = "2 + 2";
+        Expression expression = new ExpressionBuilder(calcDataString).build();
+        double result = expression.evaluate();
+        double expected = 4.0;
+        assertEquals(expected, result, 0.0001); // Delta value to handle rounding issues
+    }
+
+    @Test
+    public void testSubtraction(){
+        String calcDataString = "2 - 2";
+        Expression expression = new ExpressionBuilder(calcDataString).build();
+        double result = expression.evaluate();
+        double expected = 0.0;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test
+    public void testMultiplication(){
+        String calcDataString = "2 * 3";
+        Expression expression = new ExpressionBuilder(calcDataString).build();
+        double result = expression.evaluate();
+        double expected = 6.0;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test
+    public void testDivision(){
+        String calcDataString = "6 / 3";
+        Expression expression = new ExpressionBuilder(calcDataString).build();
+        double result = expression.evaluate();
+        double expected = 2.0;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test
+    public void testSquareRoot(){
+        double result = Math.sqrt(9);
+        double expected = 3.0;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test
+    public void testPercentage(){
+        double result = 69.0 / 100.0;
+        double expected = 0.69;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test
+    public void testFraction(){
+        double result = 1.0 / 5.0;
+        double expected = 0.2;
+        assertEquals(expected, result, 0.0001);
     }
 }
